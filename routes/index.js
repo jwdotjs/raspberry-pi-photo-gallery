c357eb186e041ad33616a8e30f4e38ba63fc06df
Jason Walsh <Jason.Walsh@colonyamerican.com>
2014-12-21 09:16:06 -0700

    Limit GET requests to the last minute, temporarily while I work on better solutions for display

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var moment = require('moment');
 
 var credentials = require('../credentials');
 var AWS = require('aws-sdk');
@@ -11,7 +12,12 @@ AWS.config.update({
 var s3 = new AWS.S3();
 
 function getImages(callback) {
-  s3.listObjects({Bucket : credentials.bucket}, function(err, data) {
+  var params = {
+    Bucket : credentials.bucket,
+    Prefix : moment().utc().format('MM-DD-YYYY_h:mm') // my pi stores files in utc format
+  };
+
+  s3.listObjects(params, function(err, data) {
     var imageNames = [];
     _.each(data.Contents, function(image) {
       imageNames.push(image.Key);
